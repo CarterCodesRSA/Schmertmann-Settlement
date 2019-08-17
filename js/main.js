7847721b7ef3290b1a2cd365f696acab77152a2c
Kyle Hoffman <hoffmankylejnr@gmail.com>
2019-08-17 11:27:13 +0200

    bug fixes with static values for entry fields for ease of qa testing

--- js/main.js
+++ js/main.js
@@ -1,216 +1,232 @@
 const config = {
-  windowWidth: 1000,
-  windowHeight: 1000,
-  foundationBase: {
-    depth: 2,
-    breadth: 4,
-    length: 4
-  },
-  IZP: 0.75,
-  type: "square",
-  satDensity: 0,
-  appLoad: 0,
-  overburden: 0,
-  waterTable: 0,
-  coneData: []
-};
+	windowWidth: 1000,
+	windowHeight: 1000,
+	foundationBase: {
+		depth: 2,
+		breadth: 4,
+		length: 4
+	},
+	IZP: 0.75,
+	type: 'square',
+	satDensity: 0,
+	appLoad: 0,
+	overburden: 0,
+	waterTable: 0,
+	coneData: []
+}
 
 window.onload = function() {
-  const topRowEl = document.getElementById("top-row");
-  const bottomRowEl = document.getElementById("bottom-row");
-  const rootRows = [bottomRowEl, topRowEl];
-  const dataTableRowsEl = document.getElementById("dataTableRows");
-  // Retrieves all inputs under the general fields div (Excluding Cone Table)
-  const generalFieldInputs = document.querySelectorAll("#general-fields input");
+	const topRowEl = document.getElementById('top-row')
+	const bottomRowEl = document.getElementById('bottom-row')
+	const rootRows = [bottomRowEl, topRowEl]
+	const dataTableRowsEl = document.getElementById('dataTableRows')
+	// Retrieves all inputs under the general fields div (Excluding Cone Table)
+	const generalFieldInputs = document.querySelectorAll('#general-fields input')
 
-  const addConeInputFields = () =>
-    rootRows.map(rootRowEl => {
-      // Create new div
-      const divEl = document.createElement("div");
-      divEl.className = "column is-1";
-      divEl.setAttribute("data-field-type", "custom");
-      // Create new input
-      const inputEl = document.createElement("input");
-      inputEl.className = "input is-small";
-      inputEl.setAttribute("type", "text");
-      inputEl.setAttribute("id", "depth-range");
-      // Append to input to div
-      divEl.appendChild(inputEl);
-      // Add to root element from array
-      rootRowEl.appendChild(divEl);
-    });
+	const addConeInputFields = () =>
+		rootRows.map(rootRowEl => {
+			// Create new div
+			const divEl = document.createElement('div')
+			divEl.className = 'column is-1'
+			divEl.setAttribute('data-field-type', 'custom')
+			// Create new input
+			const inputEl = document.createElement('input')
+			inputEl.className = 'input is-small'
+			inputEl.setAttribute('type', 'text')
+			inputEl.setAttribute('id', 'depth-range')
+			// Append to input to div
+			divEl.appendChild(inputEl)
+			// Add to root element from array
+			rootRowEl.appendChild(divEl)
+		})
 
-  const removeLastConeInputField = () => {
-    if (
-      topRowEl.children.length > 2 &&
-      topRowEl.lastChild.hasAttribute("data-field-type")
-    ) {
-      topRowEl.removeChild(topRowEl.lastChild);
-      bottomRowEl.removeChild(bottomRowEl.lastChild);
-    }
-  };
+	const removeLastConeInputField = () => {
+		if (
+			topRowEl.children.length > 2 &&
+			topRowEl.lastChild.hasAttribute('data-field-type')
+		) {
+			topRowEl.removeChild(topRowEl.lastChild)
+			bottomRowEl.removeChild(bottomRowEl.lastChild)
+		}
+	}
 
-  const processConeInputs = () => {
-    const depthInputs = Array.from(document.querySelectorAll("#top-row input"));
-    const qcInputs = Array.from(document.querySelectorAll("#bottom-row input"));
-    const coneData = [];
+	const processConeInputs = () => {
+		const depthInputs = Array.from(document.querySelectorAll('#top-row input'))
+		const qcInputs = Array.from(document.querySelectorAll('#bottom-row input'))
+		const coneData = []
 
-    for (let a = 0; a < depthInputs.length; a++) {
-      const depthValue = parseFloat(depthInputs[a].value);
-      const qcValue = parseFloat(qcInputs[a].value);
+		for (let a = 0; a < depthInputs.length; a++) {
+			const depthValue = parseFloat(depthInputs[a].value)
+			const qcValue = parseFloat(qcInputs[a].value)
 
-      // check if any field is blank, exit the function if true and prevent update
-      if (!depthValue || !qcValue) {
-        return;
-      }
-      coneData.push([depthValue, qcValue]);
-    }
-    return coneData.sort((a, b) => a[0] - b[0]);
-  };
+			// check if any field is blank, exit the function if true and prevent update
+			if (!depthValue || !qcValue) {
+				return
+			}
+			coneData.push([depthValue, qcValue])
+		}
+		return coneData.sort((a, b) => a[0] - b[0])
+	}
 
-  const processForm = () => {
-    const getGeneralFormValues = () => {
-      // Convert generalFieldInputs to array and loop over each item and adds it as a key-value pair in an object
-      return Array.from(generalFieldInputs).reduce((acc, item) => {
-        // split the ID of the input field by a hyphen
-        const inputLabel = item.id.split("-");
+	const processForm = () => {
+		const getGeneralFormValues = () => {
+			// Convert generalFieldInputs to array and loop over each item and adds it as a key-value pair in an object
+			return Array.from(generalFieldInputs).reduce((acc, item) => {
+				// split the ID of the input field by a hyphen
+				const inputLabel = item.id.split('-')
 
-        let objectLabel;
-        // if the inputLabel array is length 1 = there is no hyphen, and we simply use the ID as the destination object key
-        if (inputLabel.length === 1) {
-          objectLabel = inputLabel[0];
-        } else {
-          // if the length is > 1, we loop through the parts of the split array ['app', 'load']
-          // for every iteration after the first, we are appending the word to a string with the first letter capitalized.
-          // e.g app-load becomes appLoad - sat-density becomes satDensity
-          objectLabel = inputLabel.reduce(
-            (acc, item) => (acc += acc === "" ? item : capitalize(item)),
-            ""
-          );
-        }
-        // we append the accumulator object with a new key-value pair from our object label and input value
-        return { ...acc, [objectLabel]: parseFloat(item.value) };
-      }, {});
-    };
+				let objectLabel
+				// if the inputLabel array is length 1 = there is no hyphen, and we simply use the ID as the destination object key
+				if (inputLabel.length === 1) {
+					objectLabel = inputLabel[0]
+				} else {
+					// if the length is > 1, we loop through the parts of the split array ['app', 'load']
+					// for every iteration after the first, we are appending the word to a string with the first letter capitalized.
+					// e.g app-load becomes appLoad - sat-density becomes satDensity
+					objectLabel = inputLabel.reduce(
+						(acc, item) => (acc += acc === '' ? item : capitalize(item)),
+						''
+					)
+				}
+				// we append the accumulator object with a new key-value pair from our object label and input value
+				return { ...acc, [objectLabel]: parseFloat(item.value) }
+			}, {})
+		}
 
-    const determineType = (length, breadth) => {
-      return length / breadth >= 10 ? "strip" : "square";
-    };
+		const determineType = (length, breadth) => {
+			const selector_Strip = document.getElementById('foundationTypeStrip')
+				.checked
 
-    const fieldValues = getGeneralFormValues();
+			const selector_Square = document.getElementById('foundationTypeSquare')
+				.checked
 
-    const coneDataInputs = processConeInputs();
+			if (selector_Square) {
+				return 'square'
+			}
 
-    // descructure our keys for validation and merge with global config
-    const {
-      appLoad,
-      breadth,
-      depth,
-      dryDensity,
-      length,
-      satDensity,
-      waterTable
-    } = fieldValues;
+			if (selector_Strip) {
+				return 'strip'
+			}
+		}
 
-    /*
+		const fieldValues = getGeneralFormValues()
+
+		const coneDataInputs = processConeInputs()
+
+		// descructure our keys for validation and merge with global config
+		const {
+			appLoad,
+			breadth,
+			depth,
+			dryDensity,
+			length,
+			satDensity,
+			waterTable
+		} = fieldValues
+
+		/*
       Check for validation here with the variables declared above
     */
 
-    // check if any inputs are blank, exit the function if so
-    // all blank fields values are NaN because they are being parsed to float on line 95.
-    if (Object.values(fieldValues).includes(NaN)) {
-      return;
-    }
+		// check if any inputs are blank, exit the function if so
+		// all blank fields values are NaN because they are being parsed to float on line 95.
+		// if (Object.values(fieldValues).includes(NaN)) {
+		// 	return
+		// }
 
-    // calculate type
-    const type = determineType(length, breadth);
+		// calculate type
+		const type = determineType(length, breadth)
+		console.log('type: ', type)
 
-    // calculate overburden
-    const { overburden, q0 } = calculateOverburden({
-      foundationBase: { depth, breadth },
-      satDensity,
-      dryDensity,
-      type,
-      waterTable
-    });
+		// calculate overburden
+		const { overburden, q0 } = calculateOverburden({
+			foundationBase: { depth, breadth, length },
+			satDensity,
+			dryDensity,
+			type,
+			waterTable
+		})
+		console.log('q0: ', q0)
+		console.log('overburden: ', overburden)
 
-    // calculate IZP
-    const IZP = parseFloat(calculateIzpMax(overburden, appLoad).toFixed(3));
+		// calculate IZP
+		const IZP = parseFloat(calculateIzpMax(overburden, appLoad).toFixed(3))
 
-    //update coneData
-    const coneData = addExtraDataPoints({
-      foundationBase: { depth, breadth },
-      coneData: coneDataInputs,
-      type
-    });
+		//update coneData
+		const coneData = addExtraDataPoints({
+			foundationBase: { depth, breadth },
+			coneData: coneDataInputs,
+			type
+		})
 
-    // update our config with variables we declared above in the correct shape
-    // Object.assign takes in an object and updates it with all subsequent objects passed in as paramaters.
-    // Object.assign(objectToUpdate, newObject, { someKey: someValue })
+		// update our config with variables we declared above in the correct shape
+		// Object.assign takes in an object and updates it with all subsequent objects passed in as paramaters.
+		// Object.assign(objectToUpdate, newObject, { someKey: someValue })
 
-    const tableData = getIZPValueForEachLayer({
-      foundationBase: { breadth, depth },
-      type,
-      IZP,
-      coneData
-    });
+		const tableData = getIZPValueForEachLayer({
+			foundationBase: { breadth, depth },
+			type,
+			IZP,
+			coneData
+		})
 
-    const sum = tableData.sumVal.reduce((acc, item) => (acc = acc + item));
-    console.log("sum: ", sum);
+		const sum = tableData.sumVal.reduce((acc, item) => (acc = acc + item))
+		console.log('sum: ', sum)
 
-    const settlement = calculateSettlement(q0, appLoad, sum);
-    console.log("settlement: ", settlement);
+		const settlement = calculateSettlement(q0, appLoad, sum)
+		console.log('settlement: ', settlement)
 
-    Object.assign(config, {
-      foundationBase: { depth, length, breadth },
-      appLoad,
-      dryDensity,
-      satDensity,
-      waterTable,
-      type,
-      IZP,
-      overburden,
-      coneData,
-      q0,
-      tableData,
-      sum,
-      settlement
-    });
-  };
+		console.log('We Are here ')
+		Object.assign(config, {
+			foundationBase: { depth, length, breadth },
+			appLoad,
+			dryDensity,
+			satDensity,
+			waterTable,
+			type,
+			IZP,
+			overburden,
+			coneData,
+			q0,
+			tableData,
+			sum,
+			settlement
+		})
+	}
 
-  const buildDataTable = () => {
-    const { layer, Delta_Z, qcVal, EcVal, IzVal, sumVal } = config.tableData;
-    layer.map((item, i) => {
-      const row = document.createElement("tr");
-      row.innerHTML = `
+	const buildDataTable = () => {
+		const { layer, Delta_Z, qcVal, EcVal, IzVal, sumVal } = config.tableData
+		layer.map((item, i) => {
+			const row = document.createElement('tr')
+			row.innerHTML = `
       <td>${layer[i]}</td>
       <td>${Delta_Z[i]}</td>
       <td>${qcVal[i]}</td>
       <td>${EcVal[i]}</td>
       <td>${IzVal[i]}</td>
       <td>${sumVal[i]}</td>
-      `;
-      dataTableRowsEl.appendChild(row);
-    });
-  };
+      `
+			dataTableRowsEl.appendChild(row)
+		})
+	}
 
-  const initEventListeners = () => {
-    document
-      .getElementById("add-field")
-      .addEventListener("click", addConeInputFields);
+	const initEventListeners = () => {
+		document
+			.getElementById('add-field')
+			.addEventListener('click', addConeInputFields)
 
-    document
-      .getElementById("remove-field")
-      .addEventListener("click", removeLastConeInputField);
+		document
+			.getElementById('remove-field')
+			.addEventListener('click', removeLastConeInputField)
 
-    document
-      .getElementById("foundation-values")
-      .addEventListener("submit", e => {
-        e.preventDefault();
-        processForm();
-        buildDataTable();
-      });
-  };
-  // at the end of window.onload, initiate our event listeners
-  initEventListeners();
-};
+		document
+			.getElementById('foundation-values')
+			.addEventListener('submit', e => {
+				processForm()
+				buildDataTable()
+				e.preventDefault()
+			})
+	}
+	// at the end of window.onload, initiate our event listeners
+	initEventListeners()
+}
--- js/p5_assets/utilities.js
+++ js/p5_assets/utilities.js
@@ -1,208 +1,208 @@
 function interpolate(baseMin, baseMax, projectMin, projectMax, initialValue) {
-  const initialPositionRatio = initialValue / (baseMax - baseMin);
-  const projectPosition =
-    (projectMax - projectMin) * initialPositionRatio + projectMin;
-  return projectPosition;
+	const initialPositionRatio = initialValue / (baseMax - baseMin)
+	const projectPosition =
+		(projectMax - projectMin) * initialPositionRatio + projectMin
+	return projectPosition
 }
 
 const interpolatePercentile = (projectMin, projectMax, initialValue) =>
-  (projectMax - projectMin) * initialValue + projectMin;
+	(projectMax - projectMin) * initialValue + projectMin
 
-const limitMax = (inputValue, max) => (inputValue < max ? inputValue : max);
+const limitMax = (inputValue, max) => (inputValue < max ? inputValue : max)
 
 const calculateIzpMax = (overburden, appLoad) =>
-  0.5 + 0.1 * (appLoad / overburden) ** 0.5;
+	0.5 + 0.1 * (appLoad / overburden) ** 0.5
 
-const capitalize = word => word.charAt(0).toUpperCase() + word.substr(1);
+const capitalize = word => word.charAt(0).toUpperCase() + word.substr(1)
 
 const getMaxValueFromNestedArray = (arr, innerPosition) => {
-  let highestNumber = 0;
+	let highestNumber = 0
 
-  if (arr.length === 1) {
-    return arr[0][innerPosition];
-  } else {
-    arr.map(innerArray => {
-      if (innerArray[innerPosition] > highestNumber) {
-        highestNumber = innerArray[innerPosition];
-      }
-      return;
-    });
+	if (arr.length === 1) {
+		return arr[0][innerPosition]
+	} else {
+		arr.map(innerArray => {
+			if (innerArray[innerPosition] > highestNumber) {
+				highestNumber = innerArray[innerPosition]
+			}
+			return
+		})
 
-    return highestNumber;
-  }
-};
+		return highestNumber
+	}
+}
 function addExtraDataPoints(updatedConfig) {
-  const {
-    foundationBase: { depth, breadth },
-    type,
-    coneData
-  } = updatedConfig;
-  console.log("updatedConfig: ", updatedConfig);
+	const {
+		foundationBase: { depth, breadth },
+		type,
+		coneData
+	} = updatedConfig
+	console.log('updatedConfig: ', updatedConfig)
 
-  for (a = 0; a < coneData.length; a++) {
-    if (coneData[a][0] < depth && coneData[a + 1][0] > depth) {
-      coneData.splice(a + 1, 0, [depth, coneData[a + 1][1]]);
-    }
-    const maxDepth =
-      type === "square" ? breadth * 2 + depth : breadth * 4 + depth;
-    if (coneData[a][0] < maxDepth && coneData[a + 1][0] > maxDepth) {
-      coneData.splice(a + 1, 0, [maxDepth, coneData[a + 1][1]]);
-    }
-    const midDepth = (type == "square" ? breadth / 2 : breadth) + depth;
-    if (coneData[a][0] < midDepth && coneData[a + 1][0] > midDepth) {
-      coneData.splice(a + 1, 0, [midDepth, coneData[a + 1][1]]);
-    }
-  }
-  return coneData;
+	for (a = 0; a < coneData.length; a++) {
+		if (coneData[a][0] < depth && coneData[a + 1][0] > depth) {
+			coneData.splice(a + 1, 0, [depth, coneData[a + 1][1]])
+		}
+		const maxDepth =
+			type === 'square' ? breadth * 2 + depth : breadth * 4 + depth
+		if (coneData[a][0] < maxDepth && coneData[a + 1][0] > maxDepth) {
+			coneData.splice(a + 1, 0, [maxDepth, coneData[a + 1][1]])
+		}
+		const midDepth = (type == 'square' ? breadth / 2 : breadth) + depth
+		if (coneData[a][0] < midDepth && coneData[a + 1][0] > midDepth) {
+			coneData.splice(a + 1, 0, [midDepth, coneData[a + 1][1]])
+		}
+	}
+	return coneData
 }
 
 function calculateOverburden(updatedConfig) {
-  var {
-    foundationBase: { depth, breadth },
-    satDensity,
-    dryDensity,
-    type,
-    waterTable
-  } = updatedConfig;
+	var {
+		foundationBase: { depth, breadth },
+		satDensity,
+		dryDensity,
+		type,
+		waterTable
+	} = updatedConfig
 
-  let q0, s0;
-  const buoyantWeight = satDensity - 10;
+	let q0, s0
+	const buoyantWeight = satDensity - 10
 
-  switch (type) {
-    case "square": {
-      if (waterTable < depth) {
-        q0 = buoyantWeight;
-        s0 = buoyantWeight;
-        console.log(1);
-      } else if (waterTable >= depth && waterTable < breadth + depth) {
-        q0 = dryDensity;
-        s0 = buoyantWeight;
-        console.log(2);
-      } else {
-        q0 = dryDensity;
-        s0 = dryDensity;
-        console.log(3);
-      }
-      const overburden = q0 * depth + s0 * (breadth / 2);
-      return { overburden: Number(overburden), q0: q0 * depth };
-    }
-    case "strip":
-      if (waterTable < depth) {
-        q0 = buoyantWeight;
-        s0 = buoyantWeight;
-        console.log(4);
-      } else if (waterTable >= depth && waterTable < breadth + depth) {
-        q0 = dryDensity;
-        s0 = buoyantWeight;
-        console.log(5);
-      } else {
-        q0 = dryDensity;
-        s0 = dryDensity;
-        console.log(6);
-      }
-      const overburden = q0 * depth + s0 * breadth;
-      return { overburden: Number(overburden), q0: q0 * depth };
-  }
+	switch (type) {
+		case 'square': {
+			if (waterTable < depth) {
+				q0 = buoyantWeight
+				s0 = buoyantWeight
+				console.log(1)
+			} else if (waterTable >= depth && waterTable < breadth + depth) {
+				q0 = dryDensity
+				s0 = buoyantWeight
+				console.log(2)
+			} else {
+				q0 = dryDensity
+				s0 = dryDensity
+				console.log(3)
+			}
+			const overburden = q0 * depth + s0 * (breadth / 2)
+			return { overburden: Number(overburden), q0: q0 * depth }
+		}
+		case 'strip':
+			if (waterTable < depth) {
+				q0 = buoyantWeight
+				s0 = buoyantWeight
+				console.log(4)
+			} else if (waterTable >= depth && waterTable < breadth + depth) {
+				q0 = dryDensity
+				s0 = buoyantWeight
+				console.log(5)
+			} else {
+				q0 = dryDensity
+				s0 = dryDensity
+				console.log(6)
+			}
+			const overburden = q0 * depth + s0 * breadth
+			return { overburden: Number(overburden), q0: q0 * depth }
+	}
 }
 
 function getIZPValueForEachLayer(updatedConfig) {
-  const tableData = {
-    layer: [],
-    Delta_Z: [],
-    qcVal: [],
-    EcVal: [],
-    IzVal: [],
-    sumVal: []
-  };
+	const tableData = {
+		layer: [],
+		Delta_Z: [],
+		qcVal: [],
+		EcVal: [],
+		IzVal: [],
+		sumVal: []
+	}
 
-  const {
-    foundationBase: { breadth, depth },
-    type,
-    IZP,
-    coneData
-  } = updatedConfig;
-  console.log("updatedConfig: ", updatedConfig);
+	const {
+		foundationBase: { breadth, depth },
+		type,
+		IZP,
+		coneData
+	} = updatedConfig
+	console.log('updatedConfig: ', updatedConfig)
 
-  //addExtraDataPoints(type, depth, coneData)
+	//addExtraDataPoints(type, depth, coneData)
 
-  const yValues = [];
-  const xValues = [];
+	const yValues = []
+	const xValues = []
 
-  //Defining the major points of the graph
-  const startPoint = [type == "square" ? 0.1 : 0.2, 0],
-    midPoint = [IZP, type == "square" ? breadth / 2 : breadth],
-    endPoint = [0, type == "square" ? 2 * breadth : 4 * breadth];
+	//Defining the major points of the graph
+	const startPoint = [type == 'square' ? 0.1 : 0.2, 0],
+		midPoint = [IZP, type == 'square' ? breadth / 2 : breadth],
+		endPoint = [0, type == 'square' ? 2 * breadth : 4 * breadth]
 
-  //determine y values to provide x value pair
-  var previousDepth = 0;
+	//determine y values to provide x value pair
+	var previousDepth = 0
 
-  for (a = 0; a < coneData.length; a++) {
-    if (!previousDepth) {
-      var midDelta = coneData[a][0] / 2;
-      tableData.Delta_Z.push(parseFloat(coneData[a][0].toFixed(3)));
-      yValues.push(midDelta);
-      previousDepth = coneData[a][0];
-      continue;
-    }
-    var midDelta = (coneData[a][0] - previousDepth) / 2 + previousDepth;
-    tableData.Delta_Z.push(
-      parseFloat((coneData[a][0] - previousDepth).toFixed(3))
-    );
-    previousDepth = coneData[a][0];
-    yValues.push(midDelta);
-  }
-  for (b = 0; b < yValues.length; b++) {
-    yValues[b] = Number((yValues[b] - depth).toFixed(3));
-  }
-  //checked and working
-  startGradient = (midPoint[1] - startPoint[1]) / (midPoint[0] - startPoint[0]);
-  startYVal = startPoint[1] - startGradient * startPoint[0];
-  endGradient = (endPoint[1] - midPoint[1]) / (endPoint[0] - midPoint[0]);
-  endYVal = midPoint[1] - endGradient * midPoint[0];
+	for (a = 0; a < coneData.length; a++) {
+		if (!previousDepth) {
+			var midDelta = coneData[a][0] / 2
+			tableData.Delta_Z.push(parseFloat(coneData[a][0].toFixed(3)))
+			yValues.push(midDelta)
+			previousDepth = coneData[a][0]
+			continue
+		}
+		var midDelta = (coneData[a][0] - previousDepth) / 2 + previousDepth
+		tableData.Delta_Z.push(
+			parseFloat((coneData[a][0] - previousDepth).toFixed(3))
+		)
+		previousDepth = coneData[a][0]
+		yValues.push(midDelta)
+	}
+	for (b = 0; b < yValues.length; b++) {
+		yValues[b] = Number((yValues[b] - depth).toFixed(3))
+	}
+	//checked and working
+	startGradient = (midPoint[1] - startPoint[1]) / (midPoint[0] - startPoint[0])
+	startYVal = startPoint[1] - startGradient * startPoint[0]
+	endGradient = (endPoint[1] - midPoint[1]) / (endPoint[0] - midPoint[0])
+	endYVal = midPoint[1] - endGradient * midPoint[0]
 
-  let xVal;
+	let xVal
 
-  for (x = 0; x < yValues.length; x++) {
-    if (yValues[x] < 0) {
-      xVal = 0;
-    } else if (yValues[x] > 0 && yValues[x] < midPoint[1]) {
-      xVal = (yValues[x] - startYVal) / startGradient;
-    } else if (yValues[x] > midPoint[1] && yValues[x] < endPoint[1]) {
-      xVal = (yValues[x] - endYVal) / endGradient;
-    } else if (yValues[x] > endPoint[1]) {
-      xVal = 0;
-    }
+	for (x = 0; x < yValues.length; x++) {
+		if (yValues[x] < 0) {
+			xVal = 0
+		} else if (yValues[x] > 0 && yValues[x] < midPoint[1]) {
+			xVal = (yValues[x] - startYVal) / startGradient
+		} else if (yValues[x] > midPoint[1] && yValues[x] < endPoint[1]) {
+			xVal = (yValues[x] - endYVal) / endGradient
+		} else if (yValues[x] > endPoint[1]) {
+			xVal = 0
+		}
 
-    xVal = parseFloat(xVal.toFixed(3));
-    xValues.push(xVal);
+		xVal = parseFloat(xVal.toFixed(3))
+		xValues.push(xVal)
 
-    //this will add the layer value to the table
-    tableData.layer.push(x + 1);
-  }
-  tableData.IzVal = xValues;
+		//this will add the layer value to the table
+		tableData.layer.push(x + 1)
+	}
+	tableData.IzVal = xValues
 
-  tableData.qcVal = Object.values(coneData).map(([depth, qc]) => qc);
-  tableData.EcVal = tableData.qcVal.map(qc =>
-    parseFloat((qc * (type === "square" ? 2.5 : 3.5)).toFixed(3))
-  );
+	tableData.qcVal = Object.values(coneData).map(([depth, qc]) => qc)
+	tableData.EcVal = tableData.qcVal.map(qc =>
+		parseFloat((qc * (type === 'square' ? 2.5 : 3.5)).toFixed(3))
+	)
 
-  for (x = 0; x < tableData.layer.length; x++) {
-    tableData.sumVal.push(
-      parseFloat(
-        (
-          (tableData.IzVal[x] * tableData.Delta_Z[x]) /
-          tableData.EcVal[x]
-        ).toFixed(5)
-      )
-    );
-  }
+	for (x = 0; x < tableData.layer.length; x++) {
+		tableData.sumVal.push(
+			parseFloat(
+				(
+					(tableData.IzVal[x] * tableData.Delta_Z[x]) /
+					tableData.EcVal[x]
+				).toFixed(5)
+			)
+		)
+	}
 
-  console.log("This is tableData: ", tableData);
-  return tableData;
+	console.log('This is tableData: ', tableData)
+	return tableData
 }
 
 function calculateSettlement(q0, appLoad, sum) {
-  const c1 = 1 - 0.5 * (q0 / appLoad);
-  const settlement = c1 * appLoad * sum;
-  return settlement;
+	const c1 = 1 - 0.5 * (q0 / appLoad)
+	const settlement = c1 * appLoad * sum
+	return settlement
 }
